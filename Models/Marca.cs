5fa8ea626a7eea8e447b5829fc4073c1120f7b86
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-09-20 00:09:38 -0300

    Classe Marca

---
+++ Models/Marca.cs
@@ -0,0 +1,14 @@
+﻿namespace MarcaApi.Models
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int AnoFundacao { get; set; }
+        public string PaisOrigem { get; set; }
+        public string Setor { get; set; }
+        public string Slogan { get; set; }
+        public string Website { get; set; }
+        public string Descricao { get; set; }
+    }
+}
